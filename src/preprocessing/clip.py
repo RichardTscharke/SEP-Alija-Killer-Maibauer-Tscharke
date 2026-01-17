7186e93a87e463d7d8570b5cade7352110eed14a
RichardTscharke <r.tscharke@proton.me>
2026-01-17 03:45:01 +0100

    preprocessing/clip: pushed clip_ratio variable up to pipeline layer, added edge case checks

--- src/preprocessing/clip.py
+++ src/preprocessing/clip.py
@@ -1,4 +1,7 @@
-def clip_face(sample, clip_ratio = 0.4):
+def clip_face(sample, clip_ratio):
+
+    if not isinstance(clip_ratio, (int, float)) or clip_ratio <= 0:
+        return None
 
     image = sample["image"]
     x, y, w, h = sample["box"]
@@ -14,6 +17,10 @@ def clip_face(sample, clip_ratio = 0.4):
     x2 = min(w_img, x + w + w_clip)
     y2 = min(h_img, y + h + h_clip)
 
+    if x2 <= x1 or y2 <= y1:
+        return None
+    
     sample["box"] = (x1, y1, x2 - x1, y2 - y1)
 
     return sample
+
