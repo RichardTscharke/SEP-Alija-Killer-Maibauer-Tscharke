8c98c8255ef5c768de527710d68078e42074f5d1
RichardTscharke <r.tscharke@proton.me>
2026-01-18 22:51:30 +0100

    Implemented the function that prepares KDEF dataset.

---
+++ src/prepare_KDEF.py
@@ -0,0 +1,115 @@
+import os
+import shutil
+
+# Define paths for raw RAF dataset (Aligned & Original)
+#raw_KDEF_original_dir = "data/KDEF/Image/aligned"
+raw_KDEF_original_dir = "/Users/richardachtnull/Desktop/data/KDEF"
+
+#  Define paths for Output directories
+#output_original_dir = "data/KDEF/KDEF_original_processed"
+#output_aligned_dir = "data/KDEF/KDEF_aligned_processed"
+
+output_original_dir = "/Users/richardachtnull/Desktop/data/KDEF/Image/KDEF_original_processed"
+output_aligned_dir = "/Users/richardachtnull/Desktop/data/KDEF/Image/KDEF_aligned_processed"
+
+# Define path for output emotion-label file
+label_file = "/Users/richardachtnull/Desktop/data/KDEF/EmoLabel/list_patition_label.txt"
+
+# Define Emotion labels
+labels = {
+    1: "Surprise",
+    2: "Fear",
+    3: "Disgust",
+    4: "Happiness",
+    5: "Sadness",
+    6: "Anger",
+}
+
+def setup_directories():
+    target_dirs = [output_original_dir, output_aligned_dir]
+
+    for target_dir in target_dirs:
+        if os.path.exists(target_dir):
+            print(f"Removing existing directory: {target_dir}")
+            shutil.rmtree(target_dir)
+
+        for emotion in labels.values():
+            dir_path = os.path.join(target_dir, emotion)
+            os.makedirs(dir_path, exist_ok=True)
+            print(f"Created directory: {dir_path}")
+
+
+    label_dir = os.path.dirname(label_file)
+    os.makedirs(label_dir, exist_ok=True)
+
+    with open(label_file, "w") as f:
+        pass  # creates empty file
+
+    print(f"Created label file: {label_file}")
+
+
+def rename_and_move_files():
+    # List of possible emotion abbreviations and their corresponding emotion labels
+    emos = [("AF", 2), ("AN", 6), ("DI", 3), ("HA", 4), ("NE", 7), ("SA", 5), ("SU", 1)]
+
+    # List of fully blacked out images in the dataset we found manually
+    missing = [551, 643, 2261, 2321, 2562, 2683, 842, 3777]
+
+    # Keep a global counter for the image renaming
+    global_counter =  1
+
+    with open(label_file, "a") as lf:
+        for root, _, files in os.walk(raw_KDEF_original_dir):
+
+            # Ensures that no changes are done on the root level
+            if root == raw_KDEF_original_dir:
+                continue
+
+            files = sorted(f for f in files if f.upper().endswith(".JPG"))
+
+            # Keep a local counter for each directory in KDEF
+            local_counter = 0
+
+            for (emo, label) in emos:
+                for _ in range(5):
+
+                    if local_counter >= len(files):
+                        break
+
+                    file = files[local_counter]
+                    local_counter += 1
+
+                    if label == 7:
+                        continue
+
+                    if global_counter in missing:
+                        global_counter +=  1
+                        continue
+
+                    emotion_name = labels[label]
+
+                    # old name: <exactly 4 symbols that don't matter><emo><symbols that don't matter><".JPG">
+                    old_path = os.path.join(root,file)
+
+                    # new name: "train_<total_counter>.jpg, assuming KDEF is only used as training data"
+                    new_name = f"train_{global_counter}.jpg"
+
+                    # move to: output_original_dir/<correct emo directory determined by label>
+                    new_path = os.path.join(output_original_dir, emotion_name, new_name)
+
+                    shutil.move(old_path, new_path)
+
+                    # new entry in label_file: <new name> <label>
+                    lf.write(f"{new_name} {label}\n")
+
+                    global_counter += 1
+
+        print(f"Processed {global_counter - 1} images.")
+
+
+
+if __name__ == "__main__":
+    setup_directories()
+    rename_and_move_files()
+
+    
