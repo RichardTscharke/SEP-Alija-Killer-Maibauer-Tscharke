ecb27b6f23f942226594cb81fa16ef4bf305b23d
RichardTscharke <r.tscharke@proton.me>
2026-01-30 01:25:21 +0100

    Added models directory for history of usd models and inference calculation script.

---
+++ src/evaluation/scripts/calculate_inference.py
@@ -0,0 +1,58 @@
+import torch
+import numpy as np
+from pathlib import Path
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+from models.ResNetLight import ResNetLightCNN
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+OUTPUT_DIR = BASE_DIR / "outputs"
+
+TEST_DIR = "data/RAF_aligned_processed/test"
+BATCH_SIZE = 64
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def calculate_inference(MODEL_PATH = "models/ResNetLight_v0.pth"):
+
+    OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+
+    transform = transforms.Compose([
+        transforms.Resize((64, 64)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean = [0.5, 0.5, 0.5],
+                             std = [0.5, 0.5, 0.5]),
+    ])
+
+    test_dataset = datasets.ImageFolder(TEST_DIR, transform = transform)
+    test_loader = DataLoader(test_dataset, batch_size = BATCH_SIZE, shuffle = False)
+
+    class_names = test_dataset.classes
+    num_classes = len(class_names)
+
+    model = ResNetLightCNN(num_classes = num_classes).to(DEVICE)
+    model.load_state_dict(torch.load(MODEL_PATH, map_location = DEVICE))
+    model.eval()
+
+    all_preds = []
+    all_labels = []
+
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images = images.to(DEVICE)
+
+            outputs = model(images)
+            preds = torch.argmax(outputs, dim = 1)
+
+            all_preds.extend(preds.cpu().numpy())
+            all_labels.extend(labels.numpy())
+
+    np.save(OUTPUT_DIR / "y_pred.npy", np.array(all_preds))
+    np.save(OUTPUT_DIR / "y_true.npy", np.array(all_labels))
+    np.save(OUTPUT_DIR / "class_names.npy", np.array(class_names))
+
+    print("[INFO] Evaluation data saved:")
+    print(f" - {OUTPUT_DIR / 'y_pred.npy'}")
+    print(f" - {OUTPUT_DIR / 'y_true.npy'}")
+    print(f" - {OUTPUT_DIR / 'class_names.npy'}")
---
+++ src/models/RafCustom.py
@@ -0,0 +1,64 @@
+import torch.nn as nn
+
+
+class RafCustomCNN(nn.Module):
+    def __init__(self, num_classes=6):
+        super(RafCustomCNN, self).__init__()
+
+        # FEATURE EXTRACTOR (the "Eyes")
+        # Input: 3 Channels (RGB), 64x64 Pixel
+
+        # Block 1: 3 -> 32 Channels
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # Image reduced to 32x32
+        )
+
+        # Block 2: 32 -> 64 Channels
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),  # Image reduced to 16x16
+        )
+
+        # Block 3: 64 -> 128 Channels
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),  # Image reduced to 8x8
+        )
+
+        # Block 4: 128 -> 256 Channels
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),  # Image reduced to 4x4
+        )
+
+        # CLASSIFIER (the "Brain")
+        # at this point, the feature map size is 4x4 with 256 channels
+
+        # Flatten size = 256 * 4 * 4 = 4096
+        self.flatten = nn.Flatten()
+
+        self.fc = nn.Sequential(
+            nn.Linear(4096, 512),  # first Fully Connected Layer
+            nn.ReLU(),
+            nn.Dropout(0.5),  # Dropout for overfitting prevention
+            nn.Linear(512, num_classes),  # Output Layer (6 Neurons for 6 Emotions)
+        )
+
+    def forward(self, x, explainable=False):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+
+        x = self.flatten(x)
+        x = self.fc(x)
+        return x
