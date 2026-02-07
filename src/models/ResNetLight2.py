284530fa7ed0ca98b2029f362331bbfaf89c5bec
RichardTscharke <r.tscharke@proton.me>
2026-02-07 23:22:26 +0100

    Code Refactor. and Comments for ResNetLigh2 model.

--- src/models/ResNetLight2.py
+++ src/models/ResNetLight2.py
@@ -3,10 +3,22 @@ import torch.nn as nn
 from utils import ResidualBlock, initialize_weights
 
 class ResNetLightCNN2(nn.Module):
-    def __init__(self, num_classes=6):
-        super(ResNetLightCNN2, self).__init__()
+    '''
+    Variant of ResNetLight with fully-connected classification head.
+    Differences to Version 1:
 
-        # Initial Conv
+    -  No SE-Block in the first residual stage
+    -> Preserve early low-level features
+
+    -  Adaptive Average Pooling to 4x4 instead of 1x1
+    -> Preserve spatial details
+
+    => Goal: Capture fine-grained facial details important for FER.
+    '''
+    def __init__(self, num_classes=6):
+        super().__init__()
+
+        # Initial convolution
         self.conv1 = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(32),
@@ -14,16 +26,17 @@ class ResNetLightCNN2(nn.Module):
         )
 
         # Residual Stages
-        self.stage1 = ResidualBlock(32, 64, stride=1, use_se = False)  # SE not in Stage1
+        # Stage 1 has no SE block to avoid early channel reweighting
+        self.stage1 = ResidualBlock(32, 64, stride=1, use_se = False)
         self.stage2 = ResidualBlock(64, 128, stride=2, use_se = True)
         self.stage3 = ResidualBlock(128, 256, stride=2, use_se = True)
 
-        # Adaptive Pooling auf 4x4 statt 1x1
+        # Adaptive Pooling
         self.global_avg_pool = nn.AdaptiveAvgPool2d((4, 4))
         self.flatten = nn.Flatten()
         self.dropout = nn.Dropout(0.2)
 
-        # Fully Connected wie RafCustomCNN
+        # Fully Connected head inspired by our RafCustomCNN
         self.fc = nn.Sequential(
             nn.Linear(256*4*4, 512),
             nn.LeakyReLU(negative_slope=0.1, inplace=True),
@@ -31,13 +44,19 @@ class ResNetLightCNN2(nn.Module):
             nn.Linear(512, num_classes)
         )
 
+        # Explicit weight initialization
         initialize_weights(self)
 
     def forward(self, x):
+        # Initial feature extraction
         x = self.conv1(x)
+
+        # Residual Stages
         x = self.stage1(x)
         x = self.stage2(x)
         x = self.stage3(x)
+
+        # Global pooling and classification
         x = self.global_avg_pool(x)
         x = self.flatten(x)
         x = self.dropout(x)
