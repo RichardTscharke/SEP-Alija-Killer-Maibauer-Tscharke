b97c3289d2fa57df2f40d183fc109700663726d2
RichardTscharke <r.tscharke@proton.me>
2026-02-07 21:02:02 +0100

    Code refactor. and comments for RafCustom model.

--- src/models/RafCustom.py
+++ src/models/RafCustom.py
@@ -1,64 +1,54 @@
+import torch
 import torch.nn as nn
 
 
 class RafCustomCNN(nn.Module):
+    '''
+    Simple CNN baseline architecture.
+    Input:  (Batch, 3, 64, 64)
+    Output: (Batch, num_classes) logits
+    '''
     def __init__(self, num_classes=6):
-        super(RafCustomCNN, self).__init__()
+        super().__init__()
 
-        # FEATURE EXTRACTOR (the "Eyes")
-        # Input: 3 Channels (RGB), 64x64 Pixel
-
-        # Block 1: 3 -> 32 Channels
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # Image reduced to 32x32
+        # Standarized convolution blocks
+        def conv_block(in_c, out_c):
+            return nn.Sequential(
+                # 3x3 Convolution => Size remains the same
+                nn.Conv2d(in_c, out_c, kernel_size = 3, padding = 1),
+                # Channel-wise normalization for stabilized training
+                nn.BatchNorm2d(out_c),
+                # Nonlinearity
+                nn.ReLU(),
+                # Halfens height and width due to kernel size = 2 = stride size
+                nn.MaxPool2d(2),
+            )
+        
+        # Feature extractor: stepwise reduces the spatial resolution
+        self.features = nn.Sequential(
+            conv_block(3, 32),     # 64 -> 32
+            conv_block(32, 64),    # 32 -> 16
+            conv_block(64, 128),   # 16 -> 8
+            conv_block(128, 256),  # 8 -> 4
         )
 
-        # Block 2: 32 -> 64 Channels
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
+        # Classification head: feature maps -> logits
+        #                      (B, 256*4*4)    -> (B, 6)
+        self.classifier = nn.Sequential(
+            # Fully connected: feature maps -> neurons
+            nn.Linear(256*4*4, 512),
+            # Nonlinearity
             nn.ReLU(),
-            nn.MaxPool2d(2, 2),  # Image reduced to 16x16
+            # Prevent overfitting by randomly switching off neurons
+            nn.Dropout(0.5),
+            # Final logits per class: neurons -> logits
+            nn.Linear(512, num_classes),
         )
 
-        # Block 3: 64 -> 128 Channels
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),  # Image reduced to 8x8
-        )
-
-        # Block 4: 128 -> 256 Channels
-        self.conv4 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),  # Image reduced to 4x4
-        )
-
-        # CLASSIFIER (the "Brain")
-        # at this point, the feature map size is 4x4 with 256 channels
-
-        # Flatten size = 256 * 4 * 4 = 4096
-        self.flatten = nn.Flatten()
-
-        self.fc = nn.Sequential(
-            nn.Linear(4096, 512),  # first Fully Connected Layer
-            nn.ReLU(),
-            nn.Dropout(0.5),  # Dropout for overfitting prevention
-            nn.Linear(512, num_classes),  # Output Layer (6 Neurons for 6 Emotions)
-        )
-
-    def forward(self, x, explainable=False):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-
-        x = self.flatten(x)
-        x = self.fc(x)
-        return x
+    def forward(self, x):
+        # Extract visual features 
+        x = self.features(x)
+        # (B, 256, 4, 4) -> (Batch, 256*4*4)
+        x = torch.flatten(x, 1)
+        # Returns logits
+        return self.classifier(x)
